08554144ccee8b9489625391d69408029c57429d
Sam Spycher <samspycher@users.noreply.github.com>
2022-10-05 18:00:05 +0200

    fix: click count

--- pages/api/edge/links/[key]/clicks.ts
+++ pages/api/edge/links/[key]/clicks.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextRequest) {
   if (req.method === "GET") {
     const url = req.nextUrl.pathname;
     const key = url.split("/")[4];
-    const response = await getLinkClicksCount("dub.sh", key);
+    const response = await getLinkClicksCount("go.ginetta.net", key);
     return new Response(JSON.stringify(response), { status: 200 });
   } else {
     return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
