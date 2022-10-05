f1c8c0276c36b9a433e1be3ce3ef1ba4ac0fc68b
Sam Spycher <samspycher@users.noreply.github.com>
2022-10-05 16:12:22 +0200

    fix: disable anonymous shortening

--- pages/api/edge/links/index.ts
+++ pages/api/edge/links/index.ts
@@ -7,34 +7,36 @@ export const config = {
 
 const LINK_DOMAIN='go.ginetta.net'
 export default async function handler(req: NextRequest) {
-  if (req.method === "POST") {
-    const url = req.nextUrl.searchParams.get("url");
-    if (!url) {
-      return new Response(`Missing url or hostname`, { status: 400 });
-    }
-    const { response, key } = await setRandomKey("dub.sh", url);
-    if (response === 1) {
-      // if key was successfully added
-      await redis.zadd(`go.ginetta.net:links:timestamps:generic`, {
-        score: Date.now(),
-        member: key,
-      });
-      return new Response(
-        JSON.stringify({
-          key,
-          url,
-        }),
-        { status: 200 }
-      );
-    } else {
-      return new Response(
-        JSON.stringify({
-          error: "failed to save link",
-        }),
-        { status: 500 }
-      );
-    }
-  } else {
-    return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
-  }
+  return new Response(`Unauthorized`, { status: 401 });
+
+  // if (req.method === "POST") {
+  //   const url = req.nextUrl.searchParams.get("url");
+  //   if (!url) {
+  //     return new Response(`Missing url or hostname`, { status: 400 });
+  //   }
+  //   const { response, key } = await setRandomKey("dub.sh", url);
+  //   if (response === 1) {
+  //     // if key was successfully added
+  //     await redis.zadd(`go.ginetta.net:links:timestamps:generic`, {
+  //       score: Date.now(),
+  //       member: key,
+  //     });
+  //     return new Response(
+  //       JSON.stringify({
+  //         key,
+  //         url,
+  //       }),
+  //       { status: 200 }
+  //     );
+  //   } else {
+  //     return new Response(
+  //       JSON.stringify({
+  //         error: "failed to save link",
+  //       }),
+  //       { status: 500 }
+  //     );
+  //   }
+  // } else {
+  //   return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
+  // }
 }
