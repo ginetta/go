1d4ff7ff7ad75cc165f5f0d43aea5823e2b3bc0e
Sam Spycher <samspycher@users.noreply.github.com>
2022-10-06 12:35:05 +0200

    fix: getRandomKey based on go.ginetta.net links

--- pages/api/edge/links/random.ts
+++ pages/api/edge/links/random.ts
@@ -7,7 +7,7 @@ export const config = {
 
 export default async function handler(req: NextRequest) {
   if (req.method === "GET") {
-    const key = await getRandomKey("dub.sh");
+    const key = await getRandomKey("go.ginetta.net");
     return new Response(JSON.stringify(key), { status: 200 });
   } else {
     return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
